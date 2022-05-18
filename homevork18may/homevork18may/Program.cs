0514184cd0bc04160f32c22df15b627badba537b
yusifoa <104448485+yusifoa@users.noreply.github.com>
2022-05-18 19:13:27 +0400

    Add files via upload

---
+++ homevork18may/homevork18may/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace homevork18may
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = { 2, 3, 4, 7 };
+            int[] secondArray = { 2, 3 };
+
+            Console.WriteLine(IsArrayContains(firstArray, secondArray));
+
+        }
+        static bool IsArrayContains(int[] Array1, int[] Array2)
+        {
+            string ConvertFirstArr = string.Join(",", Array1);
+            string ConvertSecArr = string.Join(",", Array2);
+
+            if (ConvertSecArr.Length <= ConvertFirstArr.Length)
+            {
+                if (ConvertFirstArr.Contains(ConvertSecArr))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
